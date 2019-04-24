1b5a97c531aff05857fa30454cd1748ebad3e2db
Vishal Patel <vsp20@case.edu>
2019-04-24 19:35:15 -0400

    Story board commits, added initial page and main tab page. Currently no styling and trying to figure out logistics of app

---
+++ BillUp/AddPeopleVC.swift
@@ -0,0 +1,29 @@
+//
+//  AddPeopleVC.swift
+//  BillUp
+//
+//  Created by Vishal Patel on 4/24/19.
+//  Copyright © 2019 VishalAndSteve. All rights reserved.
+//
+
+import UIKit
+
+class AddPeopleVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func closePopup(_ sender: Any) {
+        dismiss(animated: true, completion:nil)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
