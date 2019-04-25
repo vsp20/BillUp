7a8e63060b7368599d38307d31480fc31a437f8c
Vishal Patel <vsp20@case.edu>
2019-04-25 10:19:17 -0400

    Added Add item popover in storyboard. Still no styling done. Buttons aren't functional yet

---
+++ BillUp/AddItemVC.swift
@@ -0,0 +1,25 @@
+//
+//  AddItemVC.swift
+//  BillUp
+//
+//  Created by Vishal Patel on 4/25/19.
+//  Copyright © 2019 VishalAndSteve. All rights reserved.
+//
+
+import UIKit
+
+class AddItemVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func closePopup(_ sender: Any) {
+        dismiss(animated: true, completion:nil)
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+
+}
